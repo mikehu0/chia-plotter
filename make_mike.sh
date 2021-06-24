0b5a2b47a361f38621d8ef9715c5c219a2edaf5e
mikehu <Mike Hu>
2021-06-24 09:33:59 +0800

    [Add] make myself with tcmalloc

---
+++ make_mike.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf build
+mkdir -p build
+
+cd build
+
+cmake -D CMAKE_CXX_FLAGS="-g -O3 -fmax-errors=1 -ltcmalloc" -DARITH="easy" -DBUILD_BLS_PYTHON_BINDINGS=false -DBUILD_BLS_TESTS=false -DBUILD_BLS_BENCHMARKS=false ..
+
+make -j8 $@
+

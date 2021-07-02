1166ddc16107dbbf0e083d9d19d99c4a3509cea6
mikehu <Mike Hu>
2021-07-02 11:43:50 +0800

    [Add] check cpu thread to make

--- make_mike.sh
+++ make_mike.sh
@@ -2,10 +2,11 @@
 
 rm -rf build
 mkdir -p build
+git submodule update --init
 
 cd build
 
 cmake -D CMAKE_CXX_FLAGS="-g -O3 -fmax-errors=1 -ltcmalloc" -DARITH="easy" -DBUILD_BLS_PYTHON_BINDINGS=false -DBUILD_BLS_TESTS=false -DBUILD_BLS_BENCHMARKS=false ..
 
-make -j8 $@
+make -j$(nproc) $@
 

8c65c5cbf5c8ef7c5d2a3e7cd5aea0721ea08d21
mikehu <Mike Hu>
2021-07-16 09:14:29 +0800

    [Change] the script's log file name

--- chia.sh
+++ chia.sh
@@ -7,7 +7,8 @@ ls -1s /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor | sed 's/0/echo per
 chmod +x cpu.sh
 sudo ./cpu.sh
 rm cpu.sh
-rm chia.log &> /dev/null
+logfile=`basename $0 | cut -d . -f 1`
+rm $logfile.log &> /dev/null
 user=`whoami`
 group=`id -gn`
 pkey="abeef2ef3751f1d3d27adbb4607b485896373a77aa8b9fca35f0e2c9469e8a103b4eb45945e0caf3842299d6d4f2acfe"
@@ -27,8 +28,8 @@ do
 			do
 				echo "$i: free space $space"
 				cd ~/chia-plotter/build;
-#				./chia_plot -p $pkey -f $fkey -t ~/chia-p/ -2 ~/chia-p/ -r $(nproc) -u 6 -n 1 >> ~/chia-log/chia.log
-				./chia_plot -c $ckey -f $fkey -t ~/chia-p/ -2 ~/chia-p/ -r $(nproc) -u 6 -n 1 >> ~/chia-log/chia.log
+#				./chia_plot -p $pkey -f $fkey -t ~/chia-p/ -2 ~/chia-p/ -r $(nproc) -u 6 -n 1 >> ~/chia-log/$logfile.log
+				./chia_plot -c $ckey -f $fkey -t ~/chia-p/ -2 ~/chia-p/ -r $(nproc) -u 6 -n 1 >> ~/chia-log/$logfile.log
 				cd ~/chia-p
 				rm *.tmp &> /dev/null
 				space=`df -B 1G $i | tail -1 | awk '{print $4}'`

ff1a9b83f167f7444121362d44c89764882d0075
Mike Hu <mikehu0@gmail.com>
2021-07-18 16:56:08 +0800

    [ADD] mem check

--- chia.sh
+++ chia.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd ~/chia-p
+foldp=~/chia-ssd
+cd $foldp
 rm *.tmp
 cd ~/chia-log
 echo "#!/bin/sh" > cpu.sh
@@ -15,6 +16,16 @@ pkey="abeef2ef3751f1d3d27adbb4607b485896373a77aa8b9fca35f0e2c9469e8a103b4eb45945
 fkey="95baed9fb5545be345db88ba607bd36d212a99d579d0c23fba6383f1fb4d73643fd847062bba2bd16d05f9e844d113a4"
 ckey="xch1dk79jxt3j40hryapv7ue54lpm0ltz7qdsut3ck2sek958rqc8cusxrehwe"
 count=$1
+mem=`free | grep -i mem | awk '{print $2}'`
+if [ $mem -gt 8000000 ];then
+	buckets=7
+elif [ $mem -gt 12000000 ];then
+	buckets=6
+else
+	echo "Not enought System RAM"
+	exit 1
+fi
+#buckets=8	//enable when use ssd
 
 for i in "$@"
 do
@@ -27,10 +38,14 @@ do
 			until [ $count == 0 ]
 			do
 				echo "$i: free space $space"
-				cd ~/chia-plotter/build;
-#				./chia_plot -p $pkey -f $fkey -t ~/chia-p/ -2 ~/chia-p/ -r $(nproc) -u 6 -n 1 >> ~/chia-log/$logfile.log
-				./chia_plot -c $ckey -f $fkey -t ~/chia-p/ -2 ~/chia-p/ -r $(nproc) -u 6 -n 1 >> ~/chia-log/$logfile.log
-				cd ~/chia-p
+				cd $foldplotter/build;
+#				./chia_plot -p $pkey -f $fkey -t $foldp/ -2 $foldp/ -r $(nproc) -u $buckets -n 1 >> ~/chia-log/$logfile.log
+				if [ $buckets == 8 ];then
+					./chia_plot -c $ckey -f $fkey -t $foldp/ -2 $foldp/ -r $(($(nproc)>>1)) -u $buckets -n 1 >> ~/chia-log/$logfile.log
+				else
+					./chia_plot -c $ckey -f $fkey -t $foldp/ -2 $foldp/ -r $(($(nproc)>>1)) -u $buckets -v 7 -n 1 >> ~/chia-log/$logfile.log
+				fi
+				cd $foldp
 				rm *.tmp &> /dev/null
 				space=`df -B 1G $i | tail -1 | awk '{print $4}'`
 				rsync --remove-source-files plot*.plot $i &

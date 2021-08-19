6f5f2fd4ab740ceefe72bfea56e58e6ccfcbc48f
mikehu <Mike Hu>
2021-08-19 11:49:48 +0800

    [Change] make_mike to release version

--- make_mike.sh
+++ make_mike.sh
@@ -6,7 +6,7 @@ git submodule update --init
 
 cd build
 
-cmake -D CMAKE_CXX_FLAGS="-O3 -fmax-errors=1 -ltcmalloc -flto -foptimize-strlen" -DARITH="easy" -DBUILD_BLS_PYTHON_BINDINGS=false -DBUILD_BLS_TESTS=false -DBUILD_BLS_BENCHMARKS=false ..
+cmake -D CMAKE_CXX_FLAGS="-O3 -fmax-errors=1 -ltcmalloc -flto -foptimize-strlen" -DARITH="easy" -DBUILD_BLS_PYTHON_BINDINGS=false ..
 
 make -j$(nproc) $@
 
